79265214c2a53513cd92a117f1b5e29ec0e691ed
Sayan Riju Chakrabarti <s26c.sayan@gmail.com>
2020-10-17 16:12:51 +0530

    Demo for using font awesome

--- src/app.js
+++ src/app.js
@@ -3,7 +3,8 @@ import React from 'react';
 function App() {
   return (
     <div>
-      Hello, world!! I am the simplest <span id="foo">React App</span> possible!
+      Hello, world!! I am the simplest <span id="foo">React App</span> possible!<br/>
+      FA Demo: <i class="fa fa-home"></i><i class="fa fa-user"></i>
     </div>
   );
 }
